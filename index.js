ef70721b8d445778464a680407b30fa3272767ac
Luke Edwards <luke.edwards05@gmail.com>
2017-01-30 07:51:16 -0800

    cleanup plugin

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 'use strict';
 
-const arrify = require('arrify');
-const bluebird = require('bluebird');
-const rimraf = require('rimraf');
+const toArr = require('arrify');
+const Promise = require('bluebird');
+const del = Promise.promisify(require('rimraf'));
 
-module.exports = function () {
-	// const del = fly.$.promisify(rimraf);
-	const del = bluebird.promisify(rimraf);
-
-	this.plugin('clear', {every: 0, files: 0}, function * (_, globs, opts) {
+module.exports = function (fly) {
+	fly.plugin('clear', {every: 0, files: 0}, function * (_, globs, opts) {
 		opts = opts || {};
-		globs = arrify(globs);
-		yield bluebird.all(globs.map(g => del(g, opts)));
+		yield Promise.all(toArr(globs).map(g => del(g, opts)));
 	});
 };
