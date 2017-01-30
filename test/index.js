d95df0daeb906c353bf5e52e4446527d87834402
Luke Edwards <luke.edwards05@gmail.com>
2017-01-30 07:52:45 -0800

    update tests

--- test/index.js
+++ test/index.js
@@ -1,98 +1,102 @@
-const join = require('path').join;
+const {join} = require('path');
 const exists = require('fs').existsSync;
 const co = require('bluebird').coroutine;
 const test = require('tape');
 const Fly = require('fly');
+const fn = require('../');
 
 const dir1 = join(__dirname, 'tmp1');
 const dir2 = join(__dirname, 'tmp2');
 
-const fly = new Fly({
-	plugins: [{
-		func: require('../')
-	}],
-	tasks: {
-		* a(o) {
-			const t = o.val.t;
-			t.true('clear' in fly, 'attach `clear` to fly global');
-			// t.true('clear' in f, 'attach `clear` to fly context');
-			yield this.clear(o.val.src);
-		}
-	}
-});
-
-const create = file => fly.$.write(file);
+const create = tasks => new Fly({tasks, plugins: [fn]});
 
 test('fly-clear: filepath (task)', co(function * (t) {
 	t.plan(2);
-	const src = `${dir1}/foo`;
-	yield create(src);
-	yield fly.start('a', {val: {t, src}});
-	t.false(exists(src), 'file was deleted');
-}));
-
-test('fly-clear: filepath', co(function * (t) {
-	t.plan(1);
-	const src = `${dir1}/foo`;
-	yield create(src);
-	yield fly.clear(src);
-	t.false(exists(src), 'file was deleted');
+	yield create({
+		* a(f) {
+			const src = `${dir1}/foo`;
+			yield f.$.write(src);
+			t.true('clear' in f, 'attach `clear` to task instance');
+			yield f.clear(src);
+			t.false(exists(src), 'file was deleted');
+		}
+	}).start('a');
 }));
 
 test('fly-clear: directory', co(function * (t) {
 	t.plan(1);
-	const src = `${dir1}/foo`;
-	yield create(src);
-	yield fly.clear(dir1);
-	t.false(exists(src), 'directory was deleted');
+	yield create({
+		* a(f) {
+			const src = `${dir1}/foo`;
+			yield f.$.write(src);
+			yield f.clear(dir1);
+			t.false(exists(src), 'directory was deleted');
+		}
+	}).start('a');
 }));
 
 test('fly-clear: filepath array', co(function * (t) {
 	t.plan(2);
-	const src1 = `${dir1}/foo`;
-	const src2 = `${dir1}/bar`;
-	yield create(src1);
-	yield create(src2);
-	yield fly.clear([src1, src2]);
-	t.false(exists(src1), 'file1 was deleted');
-	t.false(exists(src2), 'file2 was deleted');
+	yield create({
+		* a(f) {
+			const src1 = `${dir1}/foo`;
+			const src2 = `${dir1}/bar`;
+			yield f.$.write(src1);
+			yield f.$.write(src2);
+			yield f.clear([src1, src2]);
+			t.false(exists(src1), 'file1 was deleted');
+			t.false(exists(src2), 'file2 was deleted');
+		}
+	}).start('a');
 }));
 
 test('fly-clear: directory array', co(function * (t) {
 	t.plan(2);
-	const src1 = `${dir1}/foo`;
-	const src2 = `${dir2}/bar`;
-	yield create(src1);
-	yield create(src2);
-	yield fly.clear([dir1, dir2]);
-	t.false(exists(src1), 'dir1 was deleted');
-	t.false(exists(src2), 'dir2 was deleted');
+	yield create({
+		* a(f) {
+			const src1 = `${dir1}/foo`;
+			const src2 = `${dir2}/bar`;
+			yield f.$.write(src1);
+			yield f.$.write(src2);
+			yield f.clear([dir1, dir2]);
+			t.false(exists(src1), 'dir1 was deleted');
+			t.false(exists(src2), 'dir2 was deleted');
+		}
+	}).start('a');
 }));
 
 test('fly-clear: directory glob', co(function * (t) {
 	t.plan(4);
-	const src1 = `${dir1}/foo`;
-	const src2 = `${dir2}/bar/baz`;
-	const src3 = `${dir2}/bar/bat`;
-	yield create(src1);
-	yield create(src2);
-	yield create(src3);
-	yield fly.clear([dir1, `${dir2}/bar/*`]);
-	t.false(exists(src1), 'dir1 was deleted');
-	t.false(exists(src2), 'dir2/file1 was deleted');
-	t.false(exists(src3), 'dir2/file2 was deleted');
-	t.true(exists(`${dir2}/bar`), 'dir2/bar still exists');
-	yield fly.clear(dir2);
+	yield create({
+		* a(f) {
+			const src1 = `${dir1}/foo`;
+			const src2 = `${dir2}/bar/baz`;
+			const src3 = `${dir2}/bar/bat`;
+			yield f.$.write(src1);
+			yield f.$.write(src2);
+			yield f.$.write(src3);
+			yield f.clear([dir1, `${dir2}/bar/*`]);
+			t.false(exists(src1), 'dir1 was deleted');
+			t.false(exists(src2), 'dir2/file1 was deleted');
+			t.false(exists(src3), 'dir2/file2 was deleted');
+			t.true(exists(`${dir2}/bar`), 'dir2/bar still exists');
+			yield f.clear(dir2);
+		}
+	}).start('a');
 }));
 
 test('fly-clear: with `rimraf` options', co(function * (t) {
 	t.plan(2);
-	const src1 = `${dir1}/bar/baz`;
-	const src2 = `${dir1}/bar/bat`;
-	yield create(src1);
-	yield create(src2);
-	yield fly.clear(`${dir1}/bar/*`, {glob: false});
-	t.true(exists(src1), 'file1 still exists');
-	t.true(exists(src2), 'file2 still exists');
-	yield fly.clear(dir1);
+	yield create({
+		* a(f) {
+			const src1 = `${dir1}/bar/baz`;
+			const src2 = `${dir1}/bar/bat`;
+			yield f.$.write(src1);
+			yield f.$.write(src2);
+			yield f.clear(`${dir1}/bar/*`, {glob: false});
+			t.true(exists(src1), 'file1 still exists');
+			t.true(exists(src2), 'file2 still exists');
+			yield f.clear(dir1);
+		}
+	}).start('a');
 }));
